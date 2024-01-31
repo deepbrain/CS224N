e6a4eb2519aeffea22943048a35378d0b16fba4b
deepbrain <artyom@gmail.com>
2024-01-31 14:10:02 -0800

    Implemented the packing, which improved speed on fin sentiment dataset about 10x

--- math_llm.py
+++ math_llm.py
@@ -26,18 +26,17 @@ from peft import LoraConfig, get_peft_model, PeftModel
 import torch
 from loguru import logger
 from torch.utils.data import Dataset
+import random
 
 class TokenizedDataset(Dataset):
     def __init__(self, list_of_strings, tokenizer, eval = False):
         self.data = []
         self.tokenizer = tokenizer
         self.total_calls = 0
-        if eval:
-            pad = "max_length"
-            max_length = 2048
-        else:
-            pad = "do_not_pad"
-            max_length = 2048
+        self.total_length = 0
+        tokenizer.padding_side = "right"
+        pad = "do_not_pad"
+        max_length = 1024
         for s in list_of_strings:
             encoded = tokenizer(
                 text=s + tokenizer.eos_token,
@@ -46,18 +45,74 @@ class TokenizedDataset(Dataset):
                 max_length=max_length,
                 padding=pad,
             )
+            self.total_length += encoded['input_ids'].shape[1]
             self.data.append({
                 'input_ids': encoded['input_ids'].squeeze(0),
                 'labels': encoded['input_ids'].squeeze(0),
                 'attention_mask': encoded['attention_mask'].squeeze(0)
             })
+        self.mean_length = self.total_length / len(list_of_strings)
+        logger.info(f"Mean length of tokens per window: {self.mean_length}")
+        self.max_length = 1024
+        self.pack(self.max_length, 64)
+
+
+    def pack(self, max_length, N):
+        data_pack = self.data.copy()
+        self.packed_data = []
+        total_length = 0
+        while data_pack:
+            combined_item = {'input_ids': np.array([], dtype=np.int64), 'labels': np.array([], dtype=np.int64),
+                             'attention_mask': np.array([], dtype=np.int64)}
+            current_length = 0
+            items_to_remove = []
+            sample_size = min(N, len(data_pack))
+            sampled_indices = random.sample(range(len(data_pack)), sample_size)
+            random.shuffle(sampled_indices)  # Shuffle to ensure random pick order
+
+            for idx in sampled_indices:
+                item = data_pack[idx]
+                item_length = len(item['input_ids'])
+                if current_length + item_length <= max_length:
+                    for key in combined_item:
+                        combined_item[key] = np.concatenate([combined_item[key], item[key]]) if combined_item[key].size else item[key]
+                    current_length += item_length
+                    items_to_remove.append(idx)
+
+            # Padding to reach max_length if necessary
+            padding_length = max_length - current_length
+            total_length += current_length
+            if padding_length > 0:
+                pad_token_id = self.tokenizer.pad_token_id
+                for key in combined_item:
+                    combined_item[key] = np.pad(combined_item[key], (0, padding_length),
+                                                constant_values=pad_token_id)
+
+            # Ensure we always have at least one item to add to avoid empty data
+            if items_to_remove:
+                self.packed_data.append(combined_item)
+                # Remove items from self.data in reverse order to avoid index issues
+                for idx in sorted(items_to_remove, reverse=True):
+                    del data_pack[idx]
+            else:
+                # Break the loop if no items were suitable to avoid infinite loop
+                logger.warning("No items were suitable for packing, breaking loop")
+                break
+        logger.info(f"Mean length of tokens per packed window: {total_length / len(self.packed_data)}")
+        self.total_calls = 0
 
     def __len__(self):
-        return len(self.data)
+        return len(self.packed_data)
 
     def __getitem__(self, idx):
         self.total_calls += 1
-        return self.data[idx]
+        if self.total_calls > len(self.packed_data):
+            prev_len = len(self.packed_data)
+            self.pack(self.max_length, 64)
+            while len(self.packed_data) < prev_len:
+                idx = random.randint(0, len(self.packed_data) - 1)
+                self.packed_data.append(self.packed_data[idx])
+        return self.packed_data[idx]
 
 class MathLLM:
     def __init__(self, model_id, revision = "accfee56d8988cae60915486310362db5831b1bd", use_quantization = False, use_vllm = False, load = False):
@@ -67,6 +122,12 @@ class MathLLM:
         self.use_vllm = use_vllm
         self.model = None
         self.tokenizer = None
+        self.HAS_BFLOAT16 = torch.cuda.is_bf16_supported()
+        if self.HAS_BFLOAT16:
+            self.torch_dtype = torch.bfloat16
+        else:
+            self.torch_dtype = torch.float16
+
         if load:
             self.model, self.tokenizer = self.load_model(model_id, revision)
 
@@ -149,14 +210,16 @@ class MathLLM:
                     return_dict=True,
                     do_sample=True,
                     device_map="auto",
+#                    attn_implementation="flash_attention_2",
                     quantization_config=bnb_config,
-                    torch_dtype=torch.float16,
+                    torch_dtype=self.torch_dtype,
+                    #torch_dtype=torch.float16, #should this match the training dtype?
                     low_cpu_mem_usage=True,
             )
         self.model = PeftModel.from_pretrained(
             self.base_model,
             model_id,
-            torch_dtype=torch.float16,
+            torch_dtype=self.torch_dtype,
             device_map="auto",
         )
         self.model.eval()
@@ -173,7 +236,8 @@ class MathLLM:
             do_sample=True,
             device_map="auto",
             quantization_config=bnb_config,
-            torch_dtype=torch.float16,
+#            attn_implementation="flash_attention_2",
+            torch_dtype=self.torch_dtype, #should this match the training dtype?
             low_cpu_mem_usage=True,
         )
         return self.model, self.tokenizer
@@ -193,7 +257,6 @@ class MathLLM:
     def train(self, problems, eval_problems, new_model_id, lr = 2e-4, merge = False):
         # trains with a lora model on a batch of problems and saves the model to a new_model_id
         #  read the docs at https://huggingface.co/docs/trl/sft_trainer for more info
-            bnb_config = self.get_bnbs_config()
 
             if self.model_id.endswith("-lora"):
                 peft_config = PeftConfig.from_pretrained(self.model_id)
@@ -208,7 +271,9 @@ class MathLLM:
             base_model = AutoModelForCausalLM.from_pretrained(base_model_id,
                                                          revision=base_model_revision,
                                                          trust_remote_code=True,
-                                                         quantization_config=bnb_config,
+#                                                         attn_implementation="flash_attention_2",
+                                                         quantization_config=self.get_bnbs_config(),
+                                                         torch_dtype=self.torch_dtype,
                                                          device_map="auto")
             base_tokenizer = AutoTokenizer.from_pretrained(base_model_id, revision=base_model_revision, use_fast=True)
 
@@ -238,25 +303,24 @@ class MathLLM:
                 task_type="CAUSAL_LM",
             )
 
-            HAS_BFLOAT16 = torch.cuda.is_bf16_supported()
 
             training_arguments = TrainingArguments(
                 output_dir="logs",
                 num_train_epochs=4,
                 per_device_train_batch_size=1,
                 per_device_eval_batch_size=1,
-                gradient_accumulation_steps=8,  # 4
+                gradient_accumulation_steps=1,  # 4
                 optim="paged_adamw_32bit",
                 save_steps=0,
                 logging_steps=25,
-                learning_rate=2e-4,
+                learning_rate=7e-4,
                 weight_decay=0.001,
-                fp16=not HAS_BFLOAT16,
-                bf16=HAS_BFLOAT16,
+                fp16=not self.HAS_BFLOAT16,
+                bf16=self.HAS_BFLOAT16,
                 max_grad_norm=0.3,
                 max_steps=-1,
                 warmup_ratio=0.03,
-                group_by_length=True,
+                group_by_length=False,
                 lr_scheduler_type="cosine",
                 report_to="tensorboard",
                 evaluation_strategy="epoch",
@@ -269,10 +333,11 @@ class MathLLM:
                 peft_config=config,
                 tokenizer=base_tokenizer,
                 args=training_arguments,
-                packing=False,
+                packing=True,
                 dataset_text_field="text",
                 max_seq_length=2048,
             )
+
             trainer.train()
 
             if not new_model_id.endswith("-lora"):
@@ -454,18 +519,18 @@ def main():
     train_data, eval_data, X_test, y_true = load_data()
 
     logger.warning("DO NOT use quantization if your GPU has more than 20GB of memory. It is slow and can't be merged into base model.")
-    LLM = MathLLM("microsoft/phi-2", use_quantization=True, load=False)
-    LLM.train(train_data, eval_data, "phi-2-test5", merge=True) # traning takes about 40 minutes on 1 A5000
+    LLM = MathLLM("microsoft/phi-2", use_quantization=False, load=False)
+    LLM.train(train_data, eval_data, "phi-2-pad1024", merge=True) # traning takes about 40 minutes on 1 A5000
     del LLM
-    LLM = MathLLM("phi-2-test5-lora", use_quantization=True, load=True) #loading in lora mode
+    LLM = MathLLM("phi-2-pad1024-lora", use_quantization=False, load=True) #loading in lora mode
     y_pred = predict(X_test, LLM, batch_size=16)
     evaluate(y_true, y_pred)
     del LLM
-    LLM = MathLLM("phi-2-test5", use_quantization=True, load=True) #loading in regular mode
+    LLM = MathLLM("phi-2-pad1024", use_quantization=False, load=True) #loading in regular mode
     y_pred = predict(X_test, LLM, batch_size=16)
     evaluate(y_true, y_pred)
     del LLM
-    LLM = MathLLM("phi-2-test5", use_vllm=True, load=True) #loading in vllm mode
+    LLM = MathLLM("phi-2-pad1024", use_vllm=True, load=True) #loading in vllm mode
     y_pred = predict(X_test, LLM, batch_size=16)
     evaluate(y_true, y_pred)
 
