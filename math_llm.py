4a43e558a02ab843e0e042dad0a84e1ad5aeda57
Roberto09 <roberto.gt1509@gmail.com>
2024-02-02 10:30:24 -0800

    Add context_length variable in math llm and move TokenizedDataset to it's own file

--- math_llm.py
+++ math_llm.py
@@ -25,97 +25,11 @@ from peft import prepare_model_for_kbit_training
 from peft import LoraConfig, get_peft_model, PeftModel
 import torch
 from loguru import logger
-from torch.utils.data import Dataset
-import random
+from tokenized_dataset import TokenizedDataset
 BASE_PHI_REVISION = "accfee56d8988cae60915486310362db5831b1bd"
 
-class TokenizedDataset(Dataset):
-    def __init__(self, list_of_strings, tokenizer):
-        self.data = []
-        self.tokenizer = tokenizer
-        self.total_calls = 0
-        self.total_length = 0
-        tokenizer.padding_side = "right"
-        pad = "do_not_pad"
-        self.max_length = 2048
-        for s in list_of_strings:
-            encoded = tokenizer(
-                text=s + tokenizer.eos_token,
-                return_tensors="np",
-                truncation=True,
-                max_length=self.max_length,
-                padding=pad,
-            )
-            self.total_length += encoded['input_ids'].shape[1]
-            self.data.append({
-                'input_ids': encoded['input_ids'].squeeze(0),
-                'labels': encoded['input_ids'].squeeze(0),
-                'attention_mask': encoded['attention_mask'].squeeze(0)
-            })
-        self.mean_length = self.total_length / len(list_of_strings)
-        logger.info(f"Mean length of tokens per window: {self.mean_length}")
-        self.pack(64)
-
-
-    def pack(self, N):
-        data_pack = self.data.copy()
-        self.packed_data = []
-        total_length = 0
-        while data_pack:
-            combined_item = {'input_ids': np.array([], dtype=np.int64), 'labels': np.array([], dtype=np.int64),
-                             'attention_mask': np.array([], dtype=np.int64)}
-            current_length = 0
-            items_to_remove = []
-            sample_size = min(N, len(data_pack))
-            sampled_indices = random.sample(range(len(data_pack)), sample_size)
-            random.shuffle(sampled_indices)  # Shuffle to ensure random pick order
-
-            for idx in sampled_indices:
-                item = data_pack[idx]
-                item_length = len(item['input_ids'])
-                if current_length + item_length <= self.max_length:
-                    for key in combined_item:
-                        combined_item[key] = np.concatenate([combined_item[key], item[key]]) if combined_item[key].size else item[key]
-                    current_length += item_length
-                    items_to_remove.append(idx)
-
-            total_length += current_length
-            # Padding to reach max_length - not sure why, but it seems to improve accuracy
-            padding_length = self.max_length - current_length
-            if padding_length > 0:
-                pad_token_id = self.tokenizer.pad_token_id
-                for key in combined_item:
-                    combined_item[key] = np.pad(combined_item[key], (0, padding_length),
-                                                constant_values=pad_token_id)
-
-            # Ensure we always have at least one item to add to avoid empty data
-            if items_to_remove:
-                self.packed_data.append(combined_item)
-                # Remove items from self.data in reverse order to avoid index issues
-                for idx in sorted(items_to_remove, reverse=True):
-                    del data_pack[idx]
-            else:
-                # Break the loop if no items were suitable to avoid infinite loop
-                logger.warning("No items were suitable for packing, breaking loop")
-                break
-        logger.info(f"Mean length of tokens per packed window: {total_length / len(self.packed_data)}")
-        self.total_calls = 0
-
-    def __len__(self):
-        return len(self.packed_data)
-
-    def __getitem__(self, idx):
-        self.total_calls += 1
-        if self.total_calls > len(self.packed_data):
-            prev_len = len(self.packed_data)
-            self.pack(64)
-            while len(self.packed_data) < prev_len:
-                idx = random.randint(0, len(self.packed_data) - 1)
-                self.packed_data.append(self.packed_data[idx])
-        return self.packed_data[idx]
-
 class MathLLM:
-    def __init__(self, model_id, revision = None, use_quantization = False, use_vllm = False, load = False):
+    def __init__(self, model_id, revision = None, use_quantization = False, use_vllm = False, load = False, max_context_length=2048):
         self.model_id = model_id
         self.revision = revision
         self.use_quantization = use_quantization
@@ -123,6 +37,7 @@ class MathLLM:
         self.model = None
         self.tokenizer = None
         self.HAS_BFLOAT16 = torch.cuda.is_bf16_supported()
+        self.max_context_length = max_context_length
         if self.HAS_BFLOAT16:
             self.torch_dtype = torch.float16 #need to match the Phi2 training type
         else:
@@ -265,8 +180,8 @@ class MathLLM:
             base_tokenizer.pad_token = base_tokenizer.eos_token
             base_tokenizer.padding_side = "right"
 
-            train_data = TokenizedDataset(problems, base_tokenizer)
-            eval_data = TokenizedDataset(eval_problems, base_tokenizer)
+            train_data = TokenizedDataset(problems, base_tokenizer, self.max_context_length)
+            eval_data = TokenizedDataset(eval_problems, base_tokenizer, self.max_context_length)
 
 
             base_model.config.use_cache = False
@@ -328,7 +243,7 @@ class MathLLM:
                 args=training_arguments,
                 packing=False,
                 dataset_text_field="text",
-                max_seq_length=2048,
+                max_seq_length=self.max_context_length,
             )
 
             trainer.train()
